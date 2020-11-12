b3d7b772aa06edff528ca2fe78857b31fbaf2f31
Katie <katieberrym@gmail.com>
2020-11-12 13:18:03 -0800

    restructured handlesubmit and fetchTask function

--- src/ToDo.js
+++ src/ToDo.js
@@ -10,21 +10,39 @@ export default class ToDo extends Component {
     }
 
     componentDidMount = async () => {//we want to list out all the to-do itemsthe user has via feetch to db
+        await this.fetchTasks()
+    }
+
+    fetchTasks = async () => {
         await this.setState({ loading: true });
         const response = await request.get('https://tranquil-taiga-53567.herokuapp.com/api/todos')
             .set('Authorization', this.props.token)
 
-        this.setState({ todos: response.body, loading: false })
+        await this.setState({ todos: response.body, loading: false })
     }
 
     handleSubmit = async (e) => {//onSubmit, new task gets added to the list
         e.preventDefault();
-        const response = await request.post('https://tranquil-taiga-53567.herokuapp.com/api/todos')
-            .send({
-                todo: this.state.toDo,
-                priority: this.state.priorityLevel,
-            })
+
+        const newTask = {
+            todo: this.state.toDo,
+            priority: this.state.priorityLevel,
+        };
+
+        await this.setState({ loading: true });
+
+        await request.post('https://tranquil-taiga-53567.herokuapp.com/api/todos')
+            .send(newTask)
             .set('Authorization', this.props.token)
+
+        await this.fetchTasks();
+    }
+
+    handleComplete = async (taskId) => {
+        await request.put(`https://tranquil-taiga-53567.herokuapp.com/api/todos/${taskId}`)
+            .set('Authorization', this.props.token)
+
+        await this.fetchTasks();
     }
 
     render() {
@@ -33,7 +51,7 @@ export default class ToDo extends Component {
                 <h2>Welcome to your To-Do's page!</h2>
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        Create a task
+                        Create a new task
                         <input value={this.state.toDo} onChange={(e) => this.setState({ toDo: e.target.value })} />
                     </label>
                     <label>
@@ -43,10 +61,17 @@ export default class ToDo extends Component {
                     <button>Add New Task</button>
                 </form>
                 {
-                    Boolean(this.state.todos.length) && this.state.todos.map(task => <div>
-                        Task: {task.todo};
-                        Completed: {task.is_completed}
-                    </div>)
+                    this.state.loading
+                        ? 'Your Tasks Are Loading'
+                        : this.state.todos.map(task => <div key={`${task.todo}`} style={{ textDecoration: task.is_completed ? 'line-through' : 'none' }}>
+                            Task: {task.todo}
+                            {
+                                task.is_completed ? '' : <button
+                                    onClick={() => this.handleComplete(task.id)}>
+                                    Completed
+                             </button>
+                            }
+                        </div>)
                 }
             </div>
         )
